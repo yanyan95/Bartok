436e66b06e3588650f1f1eae516f29afe170ac85
yanyan95 <yl666@scarletmail.rutgers.edu>
2016-11-18 10:17:52 -0500

    ....up to Managing 2D Depth-Sorting Order of txtbook...debug....

--- Assets/__Scripts/Bartok.cs
+++ Assets/__Scripts/Bartok.cs
@@ -9,7 +9,11 @@ public class Bartok : MonoBehaviour {
     public Vector3 layoutCenter = Vector3.zero;
     //	The	number	of	degrees	to	fan	each	card	in	a	hand	
     public	float														
-        handFanDegrees	=	10f;	
+        handFanDegrees	=	10f;
+
+    public int numStartingCards = 7;
+    public float drawTimeStagger = 0.1f;
+
     public bool ________________;
     public Deck deck; public List<CardBartok> drawPile;
     public List<CardBartok> discardPile;
@@ -83,7 +87,24 @@ public class Bartok : MonoBehaviour {
             players.Add(pl);
             pl.playerNum	=	players.Count;
         }
-        players[0].type	=	PlayerType.human;   //	Make	the	0th	player	human		
+        players[0].type	=	PlayerType.human;   //	Make	the	0th	player	human	
+        CardBartok	tCB;
+        //	Deal	7	cards	to	each	player					
+        for	(int	i=0;	i<numStartingCards;	i++)	{
+            for	(int	j=0;	j<4;	j++)	{   //	There	are	always	4	players		
+                tCB	=	Draw	(); //	Draw	a	card						
+                                    //	Stagger	the	draw	time	a	bit.	Remember	order	of operations.	
+                tCB.timeStart	= Time.time	+	drawTimeStagger	*	(	i*4	+	j	);
+                //	^	By	setting	the	timeStart	before	calling	AddCard,	we		
+                //	override	the	automatic	setting	of	timeStart	in	
+                //	CardBartok.MoveTo().	
+                //	Add	the	card	to	the	player's	hand.	The	modulus	(%4)	
+                //	results	in	a	number	from	0	to	3		
+                players[	(j+1)%4	].AddCard(tCB);
+            }
+       }
+        //	Call	Bartok.DrawFirstTarget()	when	the	hand	cards	have	been drawn.
+        Invoke("DrawFirstTarget", drawTimeStagger	*	(numStartingCards*4+4)	);	
     }
     //	The	Draw	function	will	pull	a	single	card	from	the	drawPile	and return	it	
     public	CardBartok	Draw()	{
@@ -93,8 +114,23 @@ public class Bartok : MonoBehaviour {
         //	And	return	it		
     }
 
-    //	This	Update	method	is	used	to	test	adding	cards	to	players'	hands		
-    void	Update()	{
+    public void DrawFirstTarget()
+    {
+        //	Flip	up	the	first	target	card	from	the	drawPile			
+        CardBartok tCB = MoveToTarget(Draw());
+    }
+    //	This	makes	a	new	card	the	target	
+    public	CardBartok	MoveToTarget(CardBartok	tCB)	{
+        tCB.timeStart	=	0;
+        tCB.MoveTo(layout.discardPile.pos+Vector3.back);
+        tCB.state	=	CBState.toTarget;
+        tCB.faceUP	=	true;
+        targetCard	=	tCB;
+        return (tCB);
+    }
+
+        //	This	Update	method	is	used	to	test	adding	cards	to	players'	hands		
+        void Update()	{
         if	(Input.GetKeyDown(KeyCode.Alpha1))	{
             players[0].AddCard(Draw	());
         }
